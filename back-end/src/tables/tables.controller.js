9fa64d787eaec19a472d06cd4320e263218f8300
Delvon Anderson <andersondelvon@gmail.com>
2024-01-24 18:51:57 -0500

    Another commit

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -31,7 +31,30 @@ async function createTable(req, res, next) {
     res.status(201).json({ data });
 }
 
+async function seatTable(req, res, next) {
+    const { table_id } = req.params;
+    const { data } = req.body;
+
+    if (!data) {
+        return res.status(400).json({ error: "Data is missing." });
+    }
+
+    const { reservation_id } = data;
+    if (!reservation_id) {
+        return res.status(400).json({ error: "reservation_id is missing." });
+    }
+
+    await service.seatTable(table_id, reservation_id);
+    res.status(200).json({ data: { status: 'seated' } });
+}
+
+async function unseatTable(req, res, next) {
+    await service.unseatTable(req.params.table_id);
+    res.status(200).json({ data: { status: 'finished' } });
+}
 module.exports = {
     list: asyncErrorBoundary(list),
     createTable: asyncErrorBoundary(createTable),
+    seatTable: asyncErrorBoundary(seatTable),
+    unseatTable: asyncErrorBoundary(unseatTable),
 };
--- back-end/src/tables/tables.router.js
+++ back-end/src/tables/tables.router.js
@@ -2,6 +2,9 @@ const router = require('express').Router();
 const controller = require('./tables.controller');
 // const asyncErrorBoundary = require('../errors/asyncErrorBoundary');
 
+router.route('/:table_id/seat')
+    .put(controller.seatTable)
+    .delete(controller.unseatTable);
 router.route('/')
     .get(controller.list)
     .post(controller.createTable);
